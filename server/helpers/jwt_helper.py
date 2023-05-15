e128a737ee5223b23ccbc0eeec8edaf93fc01256
lemauanhphong <lemauanhphong@gmail.com>
2023-05-15 17:58:36 +0700

    Fix jwt_helper and auth api

--- server/helpers/jwt_helper.py
+++ server/helpers/jwt_helper.py
@@ -1,11 +1,11 @@
 import jwt
 
 def sign(data, private_key_location = 'key/jwtRS512.key'):
-    with open(private_key_location, 'rb').read() as file:
+    with open(private_key_location, 'rb') as file:
         private_key = file.read()
     return jwt.encode(data, private_key, algorithm="RS512").decode()
 
 def verify(token, public_key_location = 'key/jwtRS512.key.pub'):
-    with open(public_key_location, 'rb').read() as file:
+    with open(public_key_location, 'rb') as file:
         public_key = file.read()
     return jwt.decode(token, public_key, algorithms=["RS512"])
--- server/middlewares/jwt_middleware.py
+++ server/middlewares/jwt_middleware.py
@@ -2,4 +2,4 @@ from helpers import jwt_helper, key_helper
 
 def jwt_validator(token):
     data = jwt_helper.verify(token)
-    return key_helper.key_validator(data, ['username', 'exp'])
+    return key_helper.key_validator(data, ['id', 'username', 'exp'])
