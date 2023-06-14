d743e890397f85e7289b70a78f7d69ed79ef11bc
lemauanhphong <lemauanhphong@gmail.com>
2023-06-14 20:35:41 +0700

    Rewrite code to use key_helper in auth

--- server/helpers/jwt_helper.py
+++ server/helpers/jwt_helper.py
@@ -1,14 +1,16 @@
 import jwt
 
-def sign(data, private_key_location = 'key/jwtRS512.key'):
-    with open(private_key_location, 'rb') as file:
-        private_key = file.read()
-    enc = jwt.encode(data, private_key, algorithm="RS512")
+with open('key/ed25519key.pem', 'rb') as file:
+    private_key = file.read()
+
+with open('key/ed25519pubkey.pem', 'rb') as file:
+        public_key = file.read()
+
+def sign(data):
+    enc = jwt.encode(data, private_key, algorithm="EdDSA")
     if type(enc) == "bytes":
         enc = enc.decode()
     return enc
 
-def verify(token, public_key_location = 'key/jwtRS512.key.pub'):
-    with open(public_key_location, 'rb') as file:
-        public_key = file.read()
-    return jwt.decode(token, public_key, algorithms=["RS512"])
+def verify(token):
+    return jwt.decode(token, public_key, algorithms=["EdDSA"])
