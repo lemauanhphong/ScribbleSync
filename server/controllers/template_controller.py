2fe9ca56f48113e5c16bd3b742f20cd7d3f348a0
woanmeo11 <woanmeo11@gmail.com>
2023-06-16 01:25:30 +0700

    rename func

--- server/controllers/template_controller.py
+++ server/controllers/template_controller.py
@@ -1,13 +1,15 @@
-from models import template_model
 from helpers import response_helper
+from models import template_model
 
-def get_list_of_template_id():
-    list_of_id = template_model.get_list_of_template_id()
-    return (1, response_helper.response(200, -1, {'id': list_of_id}))
+
+def get_list_of_templates():
+    items = template_model.get_list_of_templates()
+    return (1, response_helper.response(200, -1, {"items": items}))
+
 
 def get_template(data):
-    template_id = int(data.split('/')[-1])
+    template_id = int(data.split("/")[-1])
     template = template_model.get_template(template_id)
     if len(template) == 0:
         return (0, response_helper.response(404, -1))
-    return (1, response_helper.response(200, -1, {'template': template[0]}))
+    return (1, response_helper.response(200, -1, {"template": template[0]}))
