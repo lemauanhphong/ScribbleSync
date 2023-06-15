66c9243bc9779d9f51d8999669f62e2ea775d19c
woanmeo11 <woanmeo11@gmail.com>
2023-06-16 00:03:48 +0700

    fix get list of id

--- server/models/template_model.py
+++ server/models/template_model.py
@@ -2,7 +2,9 @@ from database import db
 
 
 def get_list_of_template_id():
-    return db.query('SELECT id FROM templates')
+    list_id = db.query("SELECT id FROM templates")
+    return [x[0] for x in list_id]
+
 
 def get_template(template_id):
-    return db.query('SELECT * FROM templates WHERE id = %d', (template_id,))
+    return db.query("SELECT * FROM templates WHERE id = %d", (template_id,))
